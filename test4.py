aefcd0240530d5d316da3ff0a9116a03c800b67c
tieganivy3 <52673838+tieganivy3@users.noreply.github.com>
2019-07-09 19:22:29 +0100

    Create test4.py

---
+++ test4.py
@@ -0,0 +1,99 @@
+mylist=[]
+# we are define that we need a list for student names
+mylist1=[]
+# we are define that we need a list for results for fractions
+mylist2=[]
+# we are define that we need a list for results for algebra
+mylist3=[]    
+# we are define that we need a list for results for trigonometry
+print("enter username")
+username=input()
+while username !="teacher1":
+      username=input("username incorrect,try again: ")
+print("username correct, now enter password")
+password=input()
+while password !="maths123":
+    password=input("password incorrect,try again")
+print("password correct,access granted")
+# we set username and password if wrong user is asked to re-enter
+while True:
+    try:
+        r = int(input("How Many students: "))
+    except ValueError:
+        print("Sorry, But that's not a number.")
+        continue
+
+    if r not in range(1,31):
+        print("Sorry, But class size can only be between 1 and 30.")
+        continue
+    else:
+        #if the size of the class is correct.
+        #we're ready to exit the loop.
+        break
+for i in range(r):
+    mylist.append(str(input('Enter The Name Of Student: ')))
+    while True:
+          try:
+              r1 = int(input("Enter Results for fractions: "))
+          except ValueError:
+              print("Sorry, But that's not a number.")
+              continue
+
+          if r1 not in range(1,51):
+              print("Sorry, But marks for fractions only be between 1 and 50.")
+              continue
+          else:
+        #if the the result of fractions is correct.
+        #we're ready to exit the loop.
+              break
+    mylist1.append(r1)
+    while True:
+          try:
+              r2 = int(input("Enter Results for algebra: "))
+          except ValueError:
+              print("Sorry, But that's not a number.")
+              continue
+
+          if r2 not in range(1,51):
+              print("Sorry, But marks for algebra only be between 1 and 50.")
+              continue
+          else:
+        #if the the result of algebra is correct.
+        #we're ready to exit the loop.
+              break
+    mylist2.append(r2)
+    while True:
+          try:
+              r3 = int(input("Enter Results for trigonometry: "))
+          except ValueError:
+              print("Sorry, But that's not a number.")
+              continue
+
+          if r3 not in range(1,51):
+              print("Sorry, But marks for trigonometry only be between 1 and 50.")
+              continue
+          else:
+        #if the the result of trigonometry is correct.
+        #we're ready to exit the loop.
+              break
+    mylist3.append(r3)
+def Average(mylist1):
+    return sum(mylist1) / len(mylist1)
+average = Average(mylist1)
+print("Average of the fractions =", round(average, 2))
+def Average(mylist2):
+    return sum(mylist2) / len(mylist2)
+average2 = Average(mylist2)
+print("Average of the algebra =", round(average2, 2))
+def Average(mylist3):
+    return sum(mylist3) / len(mylist3)
+average3 = Average(mylist3)
+print("Average of the trigonometry =", round(average3, 2))
+# we find averange of all results and print
+print(mylist)
+print(mylist1)
+print(mylist2)
+print(mylist3)
+# we also print all of our lists
+print("Good bye")
+# we end program by saying goodbye
